c1f4d9cba2419774110234fcfd2e4a0b436aebba
Sacha Froment <sfroment42@gmail.com>
2025-06-18 15:24:13 +0200

    chore: add some rpc option
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- cmd/mojave/options.rs
+++ cmd/mojave/options.rs
@@ -11,4 +11,20 @@ pub struct Options {
         long_help = "Possible values: info, debug, trace, warn, error",
         help_heading = "Node options")]
     pub log_level: Level,
+    #[arg(
+        long = "rpc.port",
+        default_value_t = 8545,
+        value_name = "RPC_PORT",
+        help = "The port to listen for RPC requests.",
+        help_heading = "Node options"
+    )]
+    pub rpc_port: u16,
+    #[arg(
+        long = "rpc.host",
+        default_value = "0.0.0.0",
+        value_name = "RPC_HOST",
+        help = "The host to listen for RPC requests.",
+        help_heading = "Node options"
+    )]
+    pub rpc_host: String,
 }
